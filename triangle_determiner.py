4616e9694a822c8955478ea182f7c08a5c36413e
Aswad Akram <142088695+aswadakram@users.noreply.github.com>
2024-07-07 11:54:01 +0500

    Update triangle_determiner.py

--- triangle_determiner.py
+++ triangle_determiner.py
@@ -3,9 +3,9 @@ print("Triangle Determiner")
 print("===================")
 print("                    ")
 
-side1 = float(input("Enter the length of the first side: "))
-side2 = float(input("Enter the length of the second side: "))
-side3 = float(input("Enter the length of the third side: "))
+side1 = int(input("Enter the length of the first side: "))
+side2 = int(input("Enter the length of the second side: "))
+side3 = int(input("Enter the length of the third side: "))
 
 
 if side1 + side2 > side3 and side1 + side3 > side2 and side2 + side3 > side1:
